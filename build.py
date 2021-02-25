a02fecc09a49f0d5b38525711fb12ee740370e61
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-02-24 23:34:58 -0800

    Add build file

--- build.py
+++ build.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import zipfile
 import urllib.request
 from tkinter import Tk
@@ -22,7 +23,23 @@ with zipfile.ZipFile(tmp, 'r') as zip:
     zip.extractall(dir)
 
 print("Cleaning up")
-os.rename(os.path.join(dir, file), os.path.join(dir, file.split("-")[0]))
+final = os.path.join(dir, file.split("-")[0]
+os.rename(os.path.join(dir, file), final))
 os.remove(os.path.join(tmp))
+
+with open(os.path.join(final, "requirements.txt"), "r") as f:
+    packages = f.read().split("
+")
+
+if sys.platform == "windows":
+    cmd = "pip install "
+else:
+    cmd = "pip3 install "
+
+print("Installing packages")
+for package in packages:
+    if "n" in input(f"Install" + package + "? [y/n] ").lower():
+        continue
+    os.system(cmd.format(package))
 
 print("Done!")
