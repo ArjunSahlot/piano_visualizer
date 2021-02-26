d76b4bccf1894eeaf25d60f94909c970c18a5b8d
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-02-25 20:03:37 -0800

    bug fix

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 download_link=https://github.com/ArjunSahlot/piano_visualizer/archive/main.zip
 temporary_dir=$(mktemp -d)
 echo "Checking if curl is installed"
-if [ $(dpkg-query -W -f='${Status}' curl3 2>/dev/null | grep -c "ok installed") -eq 0 ];
+if [ $(sudo dpkg-query -l | grep curl | wc -l) -eq 0 ];
 then
   echo -e "\033[0;31mcurl is not installed\033[0m"
   echo "Installing curl..."
@@ -15,20 +15,21 @@ fi
 curl -LO $download_link \
 && unzip -d $temporary_dir main.zip \
 && rm -rf main.zip \
-&& mv $temporary_dir/piano_visualizer-main $1/piano_visualizer \
-&& rm -rf $temporary_dir
-echo -e "\033[0;32mSuccessfully downloaded to $1/piano_visualizer\033[0m"
-echo "Checking if pip is installed"
-if [ $(dpkg-query -W -f='${Status}' pip3 2>/dev/null | grep -c "ok installed") -eq 0 ];
+&& mkdir -p $1 \
+&& cp -r $temporary_dir/piano_visualizer-main $1/piano_visualizer \
+&& rm -rf $temporary_dir \
+&& echo -e "\033[0;32mSuccessfully downloaded to $1/piano_visualizer\033[0m" \
+&& echo "Checking if pip is installed"
+if [ $(sudo dpkg-query -l | grep python3-pip | wc -l) -eq 0 ];
 then
-  echo -e "\033[0;31mpip is not installed\033[0m"
-  echo "Installing pip..."
-  sudo apt install -y python3-pip;
-  echo -e "\033[0;32mpip was successfully installed\033[0m"
+  echo -e "\033[0;31mpip is not installed\033[0m" \
+  && echo "Installing pip..." \
+  && sudo apt install -y python3-pip \
+  && echo -e "\033[0;32mpip was successfully installed\033[0m"
 else
   echo -e "\033[0;32mpip is already installed\033[0m"
 fi
-echo "Installing requirements"
-cd $1/piano_visualizer
-pip3 install -r requirements.txt
-echo "\033[0;32mDone!\033[0m"
+echo "Installing requirements" \
+&& cd $1/piano_visualizer \
+&& pip3 install -r requirements.txt \
+&& echo -e "\033[0;32mDone!\033[0m"
