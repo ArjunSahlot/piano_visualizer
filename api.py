7f5f2d394a29755af1a7edc2c0c76bd95d72c2f0
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-07-17 16:58:22 -0700

    remove tests

--- api.py
+++ api.py
@@ -60,11 +60,13 @@ class Video:
             frac_frames = 1
 
         def quick_export(core, start, end):
-            video = cv2.VideoWriter(os.path.join(export_dir, f"video{core}.mp4"), cv2.VideoWriter_fourcc(*"MPEG"), self.fps, self.resolution)
+            video = cv2.VideoWriter(os.path.join(export_dir, f"video{core}.mp4"), cv2.VideoWriter_fourcc(
+                *"MPEG"), self.fps, self.resolution)
             for frame in range(start, end+1):
                 with open(os.path.join(export_dir, f"frame{frame}"), "w"):
                     pass
-                surf = pygame.surfarray.pixels3d(self.render(frame-self.start_offset)).swapaxes(0, 1)
+                surf = pygame.surfarray.pixels3d(self.render(
+                    frame-self.start_offset)).swapaxes(0, 1)
                 video.write(surf)
             video.release()
             cv2.destroyAllWindows()
@@ -77,9 +79,10 @@ class Video:
             print(f"Piano {i+1} done")
         print("All pianos done.")
 
-        min_frame, max_frame = min(self.pianos, key=lambda x: x.get_min_time()).get_min_time(), max(self.pianos, key=lambda x: x.get_max_time()).get_max_time()
+        min_frame, max_frame = min(self.pianos, key=lambda x: x.get_min_time()).get_min_time(
+        ), max(self.pianos, key=lambda x: x.get_max_time()).get_max_time()
 
-        max_frame = int(frac_frames *  (max_frame - min_frame) + min_frame)
+        max_frame = int(frac_frames * (max_frame - min_frame) + min_frame)
         frames = int(max_frame - min_frame)
 
         print("-"*50)
@@ -87,34 +90,41 @@ class Video:
         print(f"  Resolution: {' by '.join(map(str, self.resolution))}")
         print(f"  FPS: {self.fps}")
         print(f"  Frames: {frames}")
-        print(f"  Duration: {int((frames+self.start_offset+self.end_offset)/self.fps)} secs\n")
+        print(
+            f"  Duration: {int((frames+self.start_offset+self.end_offset)/self.fps)} secs\n")
 
         time_start = time.time()
 
         export_dir = os.path.join(pardir, "export")
         os.makedirs(export_dir, exist_ok=True)
         try:
-            video = cv2.VideoWriter(os.path.join(export_dir, "video.mp4"), cv2.VideoWriter_fourcc(*"MPEG"), self.fps, self.resolution)
+            video = cv2.VideoWriter(os.path.join(export_dir, "video.mp4"), cv2.VideoWriter_fourcc(
+                *"MPEG"), self.fps, self.resolution)
             if num_cores > 1:
                 if num_cores >= multiprocessing.cpu_count():
                     print("High chance of computer freezing")
-                    core_input = input(f"Are you sure you want to use {num_cores}: ")
+                    core_input = input(
+                        f"Are you sure you want to use {num_cores}: ")
                     try:
                         num_cores = int(core_input)
                     except ValueError:
                         if "y" in core_input.lower():
-                            print("Piano Visualizer is not at fault if your computer freezes...")
+                            print(
+                                "Piano Visualizer is not at fault if your computer freezes...")
                         else:
                             num_cores = int(input("New core count: "))
                 num_cores = min(num_cores, multiprocessing.cpu_count())
                 processes = []
                 curr_frame = 0
-                frame_inc = (frames + self.start_offset + self.end_offset) / num_cores
+                frame_inc = (frames + self.start_offset +
+                             self.end_offset) / num_cores
 
-                print(f"Exporting {int(frame_inc)} on each of {num_cores} cores...")
+                print(
+                    f"Exporting {int(frame_inc)} on each of {num_cores} cores...")
 
                 for i in range(num_cores):
-                    p = multiprocessing.Process(target=quick_export, args=(i, int(curr_frame), int(curr_frame + frame_inc)))
+                    p = multiprocessing.Process(target=quick_export, args=(
+                        i, int(curr_frame), int(curr_frame + frame_inc)))
                     p.start()
                     processes.append(p)
 
@@ -125,7 +135,7 @@ class Video:
                 with tqdm(total=frames, unit="frames", desc="Exporting") as t:
                     p = 0
                     while True:
-                        t.update((l:=len(os.listdir(export_dir)))-p)
+                        t.update((l := len(os.listdir(export_dir)))-p)
                         p = l
                         if l == frames:
                             break
@@ -133,19 +143,22 @@ class Video:
                 for i, process in enumerate(processes):
                     process.join()
 
-                videos = [os.path.join(export_dir, f"video{c}.mp4") for c in range(num_cores)]
+                videos = [os.path.join(
+                    export_dir, f"video{c}.mp4") for c in range(num_cores)]
                 with tqdm(total=frames+self.start_offset+self.end_offset+num_cores, unit="frames", desc="Concatenating") as t:
                     for v in videos:
                         curr_v = cv2.VideoCapture(v)
                         while curr_v.isOpened():
                             r, frame = curr_v.read()
-                            if not r: break
+                            if not r:
+                                break
                             video.write(frame)
                             t.update()
 
             else:
                 for frame in tqdm(range(min_frame, max_frame + self.start_offset + self.end_offset + 1), desc="Exporting", unit="frames"):
-                    surf = pygame.surfarray.pixels3d(self.render(frame-self.start_offset)).swapaxes(0, 1)
+                    surf = pygame.surfarray.pixels3d(self.render(
+                        frame-self.start_offset)).swapaxes(0, 1)
                     video.write(surf)
 
             video.release()
@@ -162,7 +175,8 @@ class Video:
                     for i, piano in enumerate(self.pianos):
                         sounds.extend(piano.gen_wavs(export_dir, millisecs))
                 else:
-                    sounds.append(AudioSegment.from_file(audio_path, format=audio_path.split(".")[-1])[0:millisecs])
+                    sounds.append(AudioSegment.from_file(
+                        audio_path, format=audio_path.split(".")[-1])[0:millisecs])
             print("Created music.")
 
             print("Combining all audios into 1...")
@@ -172,7 +186,8 @@ class Video:
                 sound = sound.overlay(i)
 
             # Compress audio to length of video
-            new_sound = sound._spawn(sound.raw_data, overrides={"frame_rate": int(sound.frame_rate*(len(sound)/millisecs))})
+            new_sound = sound._spawn(sound.raw_data, overrides={
+                                     "frame_rate": int(sound.frame_rate*(len(sound)/millisecs))})
             new_sound.set_frame_rate(sound.frame_rate)
 
             new_sound.export(music_file, format="wav")
@@ -180,15 +195,18 @@ class Video:
 
             if self.start_offset or self.end_offset:
                 print("Offsetting music...")
-                s_silent = AudioSegment.silent(self.start_offset/self.fps * 1000)
+                s_silent = AudioSegment.silent(
+                    self.start_offset/self.fps * 1000)
                 e_silent = AudioSegment.silent(self.end_offset/self.fps * 1000)
-                (s_silent + AudioSegment.from_wav(music_file) + e_silent).export(music_file, format="wav")
+                (s_silent + AudioSegment.from_wav(music_file) +
+                 e_silent).export(music_file, format="wav")
                 print("Music offsetted successfully")
 
             print("Compiling video")
             video = ffmpeg.input(os.path.join(export_dir, "video.mp4")).video
             audio = ffmpeg.input(music_file).audio
-            video = ffmpeg.output(video, audio, path, vcodec="copy", acodec="aac", strict="experimental")
+            video = ffmpeg.output(
+                video, audio, path, vcodec="copy", acodec="aac", strict="experimental")
             if os.path.isfile(path):
                 os.remove(path)
             ffmpeg.run(video)
@@ -199,16 +217,19 @@ class Video:
             print(f"Export interruputed due to {e}")
             shutil.rmtree(export_dir)
             if sys.platform == "linux" and notify:
-                os.system(f"notify-send 'Piano Visualizer' 'Export interrupted due to {e}'")
+                os.system(
+                    f"notify-send 'Piano Visualizer' 'Export interrupted due to {e}'")
             crash()
 
         shutil.rmtree(export_dir)
         total_time = time.time()-time_start
-        print(f"Finished exporting video in {total_time//60} mins and {round(total_time%60, 3)} secs.")
+        print(
+            f"Finished exporting video in {total_time//60} mins and {round(total_time%60, 3)} secs.")
         print("-"*50)
 
         if sys.platform == "linux" and notify:
-            os.system(f"notify-send 'Piano Visualizer' 'Finished exporting {path.split('/')[-1]}'")
+            os.system(
+                f"notify-send 'Piano Visualizer' 'Finished exporting {path.split('/')[-1]}'")
 
     def render(self, frame):
         surf = pygame.Surface(self.resolution, pygame.SRCALPHA)
@@ -216,7 +237,8 @@ class Video:
         width, height = self.resolution
         max_height = height/5
         min_height = height/12
-        whitekey_height = min(max_height, max(min_height, (height-100)/(len(self.pianos)+2)))
+        whitekey_height = min(max_height, max(
+            min_height, (height-100)/(len(self.pianos)+2)))
         blackkey_height = whitekey_height/2
         blackkey_width_factor = 3/4
         offset = height/len(self.pianos)
@@ -226,7 +248,8 @@ class Video:
 
         for i, piano in enumerate(self.pianos):
             p_y = offset * i
-            piano.render(surf, frame, p_y, p_width, p_height, whitekey_height, blackkey_height, whitekey_width, whitekey_width * blackkey_width_factor, 1)
+            piano.render(surf, frame, p_y, p_width, p_height, whitekey_height,
+                         blackkey_height, whitekey_width, whitekey_width * blackkey_width_factor, 1)
 
         return surf
 
@@ -254,14 +277,16 @@ class Piano:
     def render_rect(self, surf, x, y, width, height, color):
         s = pygame.Surface((width, height), pygame.SRCALPHA)
         for cy in range(int(height+1)):
-            pygame.draw.rect(s, list(color) + [255*((height-cy)/height)], (0, cy, width, 1))
+            pygame.draw.rect(s, list(color) +
+                             [255*((height-cy)/height)], (0, cy, width, 1))
         surf.blit(s, (x, y))
 
     def render(self, surf, frame, y, width, height, wheight, bheight, wwidth, bwidth, gap):
         counter = 0
         playing_keys = self.get_play_status(frame)
         black_keys = []
-        self.render_blocks(surf, frame, y, width, height - wheight, wwidth, bwidth, gap)
+        self.render_blocks(surf, frame, y, width, height -
+                           wheight, wwidth, bwidth, gap)
         py = y + height - wheight
         surf.fill((0, 0, 0), (0, py, width, wheight))
 
@@ -269,18 +294,22 @@ class Piano:
             if self.is_black(key):
                 x = (counter+1)*(wwidth + gap) - gap/2 - bwidth/2
                 if key in playing_keys:
-                    color = self.get_rainbow(x, width) if self.color == "rainbow" else self.black_hit_col
+                    color = self.get_rainbow(
+                        x, width) if self.color == "rainbow" else self.black_hit_col
                 else:
                     color = self.black_col
-                black_keys.append(((surf, self.black_col, (x, py, bwidth, bheight)), (surf, x, py, bwidth, bheight, color)))
+                black_keys.append(
+                    ((surf, self.black_col, (x, py, bwidth, bheight)), (surf, x, py, bwidth, bheight, color)))
             else:
                 counter += 1
                 x = counter*(wwidth + gap)
                 if key in playing_keys:
-                    color = self.get_rainbow(x, width) if self.color == "rainbow" else self.white_hit_col
+                    color = self.get_rainbow(
+                        x, width) if self.color == "rainbow" else self.white_hit_col
                 else:
                     color = self.white_col
-                pygame.draw.rect(surf, self.white_col, (x, py, wwidth, wheight))
+                pygame.draw.rect(surf, self.white_col,
+                                 (x, py, wwidth, wheight))
                 self.render_rect(surf, x, py, wwidth, wheight, color)
 
         for key in black_keys:
@@ -289,11 +318,14 @@ class Piano:
 
     def render_blocks(self, surf, frame, y, width, height, wwidth, bwidth, gap):
         for note in self.notes:
-            bottom = (frame - note["start"]) * self.block_speed / self.fps + y + height
-            top = bottom - (note["end"] - note["start"]) * self.block_speed / self.fps
+            bottom = (frame - note["start"]) * \
+                self.block_speed / self.fps + y + height
+            top = bottom - (note["end"] - note["start"]) * \
+                self.block_speed / self.fps
             if top <= y + height and bottom >= y:
                 x = self.get_key_x(note["note"], wwidth, gap, bwidth)
-                pygame.draw.rect(surf, self.get_rainbow(x, width) if self.color == "rainbow" else self.block_col, (x, top, bwidth if self.is_black(note["note"]) else wwidth, bottom-top), border_radius=self.block_rounding)
+                pygame.draw.rect(surf, self.get_rainbow(x, width) if self.color == "rainbow" else self.block_col, (
+                    x, top, bwidth if self.is_black(note["note"]) else wwidth, bottom-top), border_radius=self.block_rounding)
 
     def get_key_x(self, key, wwidth, gap, bwidth):
         counter = 1
@@ -329,7 +361,8 @@ class Piano:
                     else:
                         if msg.type in ("note_on", "note_off"):
                             if not msg.velocity or msg.type == "note_off":
-                                self.notes.append({"note": msg.note - 21, "start": start_keys[msg.note - 21], "end": int(frame)})
+                                self.notes.append(
+                                    {"note": msg.note - 21, "start": start_keys[msg.note - 21], "end": int(frame)})
                             else:
                                 start_keys[msg.note - 21] = int(frame)
 
@@ -355,10 +388,12 @@ class Piano:
         for midi in self.midis:
             os.system(f"timidity {midi} -Ow -o {wav_path}")
             try:
-                wavs.append(a:=AudioSegment.from_wav(wav_path))
+                wavs.append(a := AudioSegment.from_wav(wav_path))
             except FileNotFoundError:
-                print("You might not have timidity installed on your machine.", file=sys.stderr)
-                print("Please have that installed if you are using the midi files as audio.", file=sys.stderr)
+                print(
+                    "You might not have timidity installed on your machine.", file=sys.stderr)
+                print(
+                    "Please have that installed if you are using the midi files as audio.", file=sys.stderr)
                 return [AudioSegment.silent(silent_len)]
         return wavs
 
@@ -366,9 +401,3 @@ class Piano:
         self.fps = fps
         self.offset = offset
         self.parse_midis()
-
-
-p = Piano(["/home/arjun/asdf.mid"], True, "rainbow")
-v = Video(fps=30, start_offset=30, end_offset=30)
-v.add_piano(p)
-v.export("/home/arjun/asdf.mp4", 6, frac_frames=1)
